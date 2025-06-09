eb26043c96723eddc4cea8e338adb2d24387798b
Peter Frentrup <peter_frentrup@gmx.de>
2025-06-09 12:14:04 +0200

    "Lock widget size" better reflects the items purpose

--- NppMenuSearch/Localization/LocalizedStrings.cs
+++ NppMenuSearch/Localization/LocalizedStrings.cs
@@ -11,12 +11,12 @@ namespace NppMenuSearch.Localization
         public string MenuTitle_RepeatCommand_Previous = "Repeat previous command";
         public string MenuTitle_RepeatCommand_arg = "Repeat command “{0}”";
         public string MenuTitle_About = "&About...";
-        public string MenuTitle_FixWidgetSize = "&Fix widget size";
+        public string MenuTitle_FixWidgetSize = "&Lock widget size";
         public string GroupTitle_RecentlyUsed = "Recently Used";
         public string GroupTitle_Menu = "Menu";
         public string GroupTitle_Preferences = "Preferences";
         public string GroupTitle_OpenFiles = "Open Files";
-        public string SwitchGroupHelpText = "TAB switches groups: Recently Used ↔ Menu ↔ Open Files ↔ Preferences";
+        public string SwitchGroupHelpText = "TAB switches groups: Recently Used ↔ Menu ↔ Open Files ↔ Preferences.";
         public string ShortcutHelpText_arg = "Press {0} again for all results.";
     }
 }
