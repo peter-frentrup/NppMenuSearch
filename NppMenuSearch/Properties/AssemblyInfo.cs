cb61e1418c531b3827fd096022eed77c776fb3d2
Peter Frentrup <peter_frentrup@gmx.de>
2025-06-12 09:51:56 +0200

    version 0.9.7

--- NppMenuSearch/Properties/AssemblyInfo.cs
+++ NppMenuSearch/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NppMenuSearch")]
-[assembly: AssemblyCopyright("Copyright © Peter Frentrup 2013")]
+[assembly: AssemblyCopyright("Copyright © Peter Frentrup 2013-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6.*")]
-[assembly: AssemblyFileVersion("0.9.6.0")]
+[assembly: AssemblyVersion("0.9.7.*")]
+[assembly: AssemblyFileVersion("0.9.7.0")]
