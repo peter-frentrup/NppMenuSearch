c9120be0b984a1be4449a9f281f2bb4a5c68afde
Anton Mamontov <anton_nix@mail.ru>
2025-11-02 16:20:05 -0800

    Try UnmanagedExports.Repack.Upgrade

--- NppMenuSearch/UnmanagedExports.cs
+++ NppMenuSearch/UnmanagedExports.cs
@@ -7,34 +7,34 @@ namespace NppMenuSearch
 {
     class UnmanagedExports
     {
-        [DllExport(nameof(isUnicode), CallingConvention.Cdecl)]
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
         static bool isUnicode()
         {
             return true;
         }
 
-        [DllExport(nameof(setInfo), CallingConvention.Cdecl)]
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
         static void setInfo(NppData notepadPlusData)
         {
             PluginBase.nppData = notepadPlusData;
             Main.CommandMenuInit();
         }
 
-        [DllExport(nameof(getFuncsArray), CallingConvention.Cdecl)]
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
         static IntPtr getFuncsArray(ref int nbF)
         {
             nbF = PluginBase._funcItems.Items.Count;
             return PluginBase._funcItems.NativePointer;
         }
 
-        [DllExport(nameof(messageProc), CallingConvention.Cdecl)]
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
         static uint messageProc(uint Message, IntPtr wParam, IntPtr lParam)
         {
             return 1;
         }
 
         static IntPtr _ptrPluginName = IntPtr.Zero;
-        [DllExport(nameof(getName), CallingConvention.Cdecl)]
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
         static IntPtr getName()
         {
             if (_ptrPluginName == IntPtr.Zero)
@@ -42,7 +42,7 @@ namespace NppMenuSearch
             return _ptrPluginName;
         }
 
-        [DllExport(nameof(beNotified), CallingConvention.Cdecl)]
+        [DllExport(CallingConvention = CallingConvention.Cdecl)]
         static void beNotified(IntPtr notifyCode)
         {
             SCNotification nc = (SCNotification)Marshal.PtrToStructure(notifyCode, typeof(SCNotification));
